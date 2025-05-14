7dc6d984cb14e691c0e23231a3e84fd249c8dcb1
1DECRATE1 <nukegame12@gmail.com>
2025-05-14 21:37:40 +0300

    now header close when click a link

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -43,6 +43,7 @@ export default function Header() {
             alt="Logo"
             style={{ objectFit: "contain" }}
             className="drop-shadow-[0_0px_10px_rgba(0,0,0,0.25)]"
+            onClick={() => setMenuIsHide(false)}
           ></Image>
         </Link>
       )}
